a3ef96cb1c4f3847d5630276757766bb12399ba9
Riviera Sperduto <riviera.t.sperduto13@gmail.com>
2025-02-12 16:13:45 -0500

    Finished the lab activity

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -17,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         // printing their output to the Log, which is visible in the LogCat:
         // eg. Log.d("function output", getTestDataArray().toString())
 
+        Log.d("function output", getTestDataArray2().toString())
+        Log.d("function output: ", averageLessThanMedian2(getTestDataArray2()).toString())
+        Log.d("function output",
+            getView2(position = 1, recycledView = null, collection = getTestDataArray2(), context = this).toString()
+        )
     }
 
 
@@ -33,6 +38,9 @@ class MainActivity : AppCompatActivity() {
         return testArray
     }
 
+    private fun getTestDataArray2(): List<Int> =
+        List(10) { Random.nextInt() }.sorted()
+
     // Return true if average value in list is greater than median value, false otherwise
     private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
         val avg = listOfNumbers.average()
@@ -45,21 +53,37 @@ class MainActivity : AppCompatActivity() {
         return avg < median
     }
 
-    // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
-    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
-        val textView: TextView
-
-        if (recycledView != null) {
-            textView = recycledView as TextView
-        } else {
-            textView = TextView(context)
-            textView.setPadding(5, 10, 10, 0)
-            textView.textSize = 22f
+    private fun averageLessThanMedian2(listOfNumbers: List<Int>): Boolean =
+        listOfNumbers.average() < listOfNumbers.sorted().let { sortedList ->
+            if (sortedList.size % 2 == 0)
+                (sortedList[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
+            else
+                sortedList[sortedList.size / 2]
         }
 
-        textView.text = collection[position].toString()
 
-        return textView
+
+}// Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
+private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
+    val textView: TextView
+
+    if (recycledView != null) {
+        textView = recycledView as TextView
+    } else {
+        textView = TextView(context)
+        textView.setPadding(5, 10, 10, 0)
+        textView.textSize = 22f
     }
 
+    textView.text = collection[position].toString()
+
+    return textView
 }
+
+private fun getView2(position: Int, recycledView: View?, collection: List<Int>, context: Context): View =
+    (recycledView as? TextView ?: TextView(context).apply {
+        setPadding(5, 10, 10, 0)
+        textSize = 22f
+    }).apply {
+        text = collection[position].toString()
+    }
